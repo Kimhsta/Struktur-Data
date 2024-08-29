26ad0096ad36c3d156e3351bed7d6abeb46a7f03
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-08-30 01:09:41 +0700

    ah mbuh

---
+++ Array/Contoh Simpel/mengolah10-data.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // KAMUS
+    string nama1, nama2, nama3, nama4, nama5;
+    string nama6, nama7, nama8, nama9, nama10;
+
+    // ALGORITMA: Menerima 10 nama dari pengguna
+    cout << "Masukkan nama-1: ";
+    cin >> nama1;
+    cout << "Masukkan nama-2: ";
+    cin >> nama2;
+    cout << "Masukkan nama-3: ";
+    cin >> nama3;
+    cout << "Masukkan nama-4: ";
+    cin >> nama4;
+    cout << "Masukkan nama-5: ";
+    cin >> nama5;
+    cout << "Masukkan nama-6: ";
+    cin >> nama6;
+    cout << "Masukkan nama-7: ";
+    cin >> nama7;
+    cout << "Masukkan nama-8: ";
+    cin >> nama8;
+    cout << "Masukkan nama-9: ";
+    cin >> nama9;
+    cout << "Masukkan nama-10: ";
+    cin >> nama10;
+
+    // Menampilkan semua kombinasi pasangan nama
+    cout << nama1 << " - " << nama2 << endl;
+    cout << nama1 << " - " << nama3 << endl;
+    cout << nama1 << " - " << nama4 << endl;
+    cout << nama1 << " - " << nama5 << endl;
+    cout << nama1 << " - " << nama6 << endl;
+    cout << nama1 << " - " << nama7 << endl;
+    cout << nama1 << " - " << nama8 << endl;
+    cout << nama1 << " - " << nama9 << endl;
+    cout << nama1 << " - " << nama10 << endl;
+
+    cout << nama2 << " - " << nama3 << endl;
+    cout << nama2 << " - " << nama4 << endl;
+    cout << nama2 << " - " << nama5 << endl;
+    cout << nama2 << " - " << nama6 << endl;
+    cout << nama2 << " - " << nama7 << endl;
+    cout << nama2 << " - " << nama8 << endl;
+    cout << nama2 << " - " << nama9 << endl;
+    cout << nama2 << " - " << nama10 << endl;
+
+    cout << nama3 << " - " << nama4 << endl;
+    cout << nama3 << " - " << nama5 << endl;
+    cout << nama3 << " - " << nama6 << endl;
+    cout << nama3 << " - " << nama7 << endl;
+    cout << nama3 << " - " << nama8 << endl;
+    cout << nama3 << " - " << nama9 << endl;
+    cout << nama3 << " - " << nama10 << endl;
+
+    cout << nama4 << " - " << nama5 << endl;
+    cout << nama4 << " - " << nama6 << endl;
+    cout << nama4 << " - " << nama7 << endl;
+    cout << nama4 << " - " << nama8 << endl;
+    cout << nama4 << " - " << nama9 << endl;
+    cout << nama4 << " - " << nama10 << endl;
+
+    cout << nama5 << " - " << nama6 << endl;
+    cout << nama5 << " - " << nama7 << endl;
+    cout << nama5 << " - " << nama8 << endl;
+    cout << nama5 << " - " << nama9 << endl;
+    cout << nama5 << " - " << nama10 << endl;
+
+    cout << nama6 << " - " << nama7 << endl;
+    cout << nama6 << " - " << nama8 << endl;
+    cout << nama6 << " - " << nama9 << endl;
+    cout << nama6 << " - " << nama10 << endl;
+
+    cout << nama7 << " - " << nama8 << endl;
+    cout << nama7 << " - " << nama9 << endl;
+    cout << nama7 << " - " << nama10 << endl;
+
+    cout << nama8 << " - " << nama9 << endl;
+    cout << nama8 << " - " << nama10 << endl;
+
+    cout << nama9 << " - " << nama10 << endl;
+
+    return 0;
+}
