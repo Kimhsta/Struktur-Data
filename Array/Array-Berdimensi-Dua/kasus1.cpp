c0c8ecc5367a64c0e0f7d19f42058a2d61752d1e
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-09-04 14:45:42 +0700

    Membuat Data nilai siswa perkelas seperti tabel

---
+++ Array/Array-Berdimensi-Dua/kasus1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai[5][2] = {{100, 90},
+                       {90, 80},
+                       {78, 100},
+                       {90, 90},
+                       {66, 100}};
+
+    int baris, kolom;
+    cout << "Isi dari array dua dimensi nilai[5][2] : \n";
+    for (baris = 0; baris < 5; baris++)
+    {
+        for (kolom = 0; kolom < 2; kolom++)
+        {
+            cout << nilai[baris][kolom] << " , ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
