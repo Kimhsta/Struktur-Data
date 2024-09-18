d526ac322b1ffe8706ba6419666e53c9c777dcc1
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-09-19 00:50:31 +0700

    mencetak nilai dan alamat memori dari dua variabel, yaitu buku dan pulpen

---
+++ TypeData-Pointer/contoh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+main()
+{
+    int buku = 5;
+    int pulpen = 6;
+
+    cout << "Nilai dari variabel buku : " << buku;
+    cout << endl;
+    cout << "Alamat dari variabel buku : " << &buku;
+    cout << endl;
+    cout << "Nilai dari variabel pulpen : " << pulpen;
+    cout << endl;
+    cout << "Alamat dari variabel pulpen : " << &pulpen;
+    cout << endl;
+    getch();
+}
